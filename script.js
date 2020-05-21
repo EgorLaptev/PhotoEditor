7ae69eee97c4a8495984f2a3a0b002e4299b5d54
Egor Laptev <egor.lapteffff@gmail.com>
2020-05-21 14:31:16 +0300

    21.05.2020 14:31

--- script.js
+++ script.js
@@ -4,48 +4,71 @@
 let cnv 	= document.getElementById('canv'),
 		ctx		= cnv.getContext('2d');
 
+// Canvas's sizes
 cnv.width   = window.innerWidth;
-cnv.height  = window.innerWidth;
+cnv.height  = window.innerHeight;
+
 
 // Image
-let img = new Image(cnv.width, cnv.width/1.6);
+let img = new Image();
 
-img.dx 	= 0;
-img.dy 	= 0;
-img.sx 	= 0;
-img.sy 	= 0;
-img.dw 	= img.width;
-img.dh 	= img.height;
-img.sw 	= img.width;
-img.sh 	= img.height;
+// Image's properties
+img.isLoaded = false;
 
-let uploadForm	= document.getElementById('uploadForm'),
+img.onload = function() {
+	img.dx 	= 0;
+	img.dy 	= 0;
+	img.sx 	= 0;
+	img.sy 	= 0;
+	img.sw 	= img.width;
+	img.sh 	= img.height;
+	img.dw 	= cnv.width;
+	img.dh 	= cnv.width/(img.sw / img.sh);
+	img.isLoaded = true;
+}
+
+
+// Upload a image
+let dropArea		= document.getElementById('image-label'),
 		userImage 	= document.getElementById('image');
 
-userImage.addEventListener('change', (e)=>{
+// Canсel the propagtion for drag'n'drop
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => { dropArea.addEventListener(eventName, (e)=>{e.preventDefault();e.stopPropagation()}, false) });
 
-	// Достаю файл из формы
-	let file = userImage.files[0];
+// Styling drag'n'drop
+dropArea.addEventListener('dragleave', unhighlight, false);
+dropArea.addEventListener('dragenter', highlight, false);
 
-	// "Читаю" полученную картнку
+function highlight(e) { dropArea.classList.add('highlight') }
+function unhighlight(e) { dropArea.classList.remove('highlight') }
+
+// processing drag'n'drop
+dropArea.addEventListener('drop', uploadImage)
+// processing upload from input
+userImage.addEventListener('change', uploadImage);
+
+function uploadImage(e) {
+
+	// Take a file from input or using drag'n'drop
+	let file = userImage.files[0] || e.dataTransfer.files[0];
+
+	// "Read" received image
 	let reader = new FileReader();
 			reader.readAsDataURL(file);
 			reader.onload = function() {
-
-				// Если загружена картинка, то...
-				if(reader.result.search(/data:image\/jpeg;/) != -1) {
-					// Новый путь к картинке,
+				// If the image is successfully loaded, then..
+				if(reader.result.search(/data:image\/jpeg|png|ico|tiff|webp|eps|svg|psd|indd|cdr|ai|raw;/) != -1) {
+					// New image's source,
 					img.src = reader.result;
 
 					draw();
 
-					// Прячу форму после удачной загрузки картинки
-					uploadForm.style.display = 'none';
+					// Hide the form after successfully uploaded
+					dropArea.style.display = 'none';
 				}
-
 			};
+}
 
-});
 
 // CutArea
 let cutArea = {
@@ -55,6 +78,7 @@ let cutArea = {
 	h: 0,
 	v: false
 }
+
 
 function draw() {
 
@@ -70,7 +94,6 @@ function draw() {
 
 	requestAnimationFrame(draw)
 }
-
 function drawCutArea() {
 	ctx.fillStyle 	= 'rgba(75, 75, 250, 0.3)';
 	ctx.strokeStyle = 'rgba(50, 50, 100, 0.8)';
@@ -79,37 +102,39 @@ function drawCutArea() {
 	ctx.stroke();
 }
 
+
+// Cropped image properties
 let newImg = {};
 
 document.addEventListener('mousedown', (e)=>{
 
-	if(img.src != "") {
-		newImg.sx	= e.x;
-		newImg.sy 	= e.y;
+	if(img.isLoaded) {
+		newImg.sx	= e.x * (img.width/cnv.width);
+		newImg.sy = e.y * (img.height/cnv.height);
 
-		cutArea.x	= e.x;
+		cutArea.x		= e.x;
 		cutArea.y 	= e.y;
 		cutArea.v 	= true;
 	}
 
 });
-
 document.addEventListener('mousemove', (e)=>{
 
 	cutArea.w 	= e.x - cutArea.x;
 	cutArea.h 	= e.y - cutArea.y;
 
 });
-
 document.addEventListener('mouseup', (e)=>{
-	if(img.src != "") {
-		newImg.sw 	= e.x - newImg.sx;
-		newImg.sh 	= e.y - newImg.sy;
-		newImg.dw 	= newImg.sw;
-		newImg.dh 	= newImg.sh;
-	}
-	Object.assign(img, newImg);
 
-	cutArea.v = false;
+	if(img.isLoaded) {
+		newImg.sw = e.x * (img.width/cnv.width) - newImg.sx;
+		newImg.sh = e.y * (img.height/cnv.height) - newImg.sy;
+		newImg.dw	=	newImg.sw / (img.width/cnv.width);
+		newImg.dh	= newImg.sh / (img.height/cnv.height);
+
+		Object.assign(img, newImg);
+
+		cutArea.v = false;
+	}
 
 });
